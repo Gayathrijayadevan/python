b16a5b82f3be42b3c38fbdbfc1b70f8dfc8e44af
Gayathrijayadevan <gayathrijayadevan11@gmail>
2024-06-21 13:41:22 +0530

    python friday

---
+++ sample.py
@@ -0,0 +1,30 @@
+# print("welcome to all")
+# print('hello')
+# name="Gayathri"
+# age=21
+# weight=60
+# print("name:",name)
+# print("age:",age)
+
+'''a=50+5j
+print(type(a))'''
+
+'''a="name"
+print(type(a))'''
+# a=True
+# print(type(a))
+
+# a= [ 10,20,'abc']
+# print(type(a))
+
+# f=(10,20,'abd')
+# print(type(f))
+
+# s={10,20,50}
+# print(type(s))
+
+d={'name':'gayu'}
+print(type(d))
+
+
+

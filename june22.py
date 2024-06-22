0c88877cc74430caee62ee9d181f24ce0aed0e60
Gayathrijayadevan <gayathrijayadevan11@gmail.com>
2024-06-22 11:27:29 +0530

    saturday 22june

---
+++ june22.py
@@ -0,0 +1,178 @@
+# name=(input("enter name"))
+# age=int(input("enter age"))
+# weight=float(input("enter weight"))
+
+# print("name:",name)
+# print("age:",age)
+# print("weight:",weight)
+
+# print(name,'\n',age,'\n',weight)
+
+# a=int(input("enter first number"))
+# b=int(input("enter second number"))
+# c=a+b
+# print(c)
+
+# print(a+b)
+
+# a='welcome'
+# b='arya'
+# c=a+b
+
+# print(c)
+
+# a=10
+# b=5
+# c=a-b
+# print(a-b)
+
+# a=10
+# b=10
+# c=a*b
+# print(a*b)
+
+# a='ha'
+# print(a*2)
+
+# a=10
+# b=10
+# c=a/b
+# print(a/b)
+
+# a=10
+# b=10
+# c=a%b
+# print(a%b)
+
+# a=10
+# b=5
+# c=a**b
+# print(a**b)
+
+# a=10
+# b=5
+# c=a//b
+# print(a//b)
+
+# a=10
+# a+=5
+# print(a)
+
+# a=10
+# a-=5
+# print(a)
+
+# a=10
+# a*=5
+# print(a)
+
+# a=10
+# a%=5
+# print(a)
+
+# a=10
+# a/=5
+# print(a)
+
+# a=10
+# a//=5
+# print(a)
+
+# a=int(input("enter a number"))
+# a**=5
+# print(a)
+
+# a=10
+# b=20
+# print(a==b)
+
+
+# a=10
+# b=20
+# print(a!=b)
+
+
+# a=10
+# b=20
+# print(a<b)
+
+
+# a=10
+# b=6
+# print(a>b)
+
+
+# a=10
+# b=20
+# print(a<=b)
+
+
+# a=int(input("enter first number"))
+# b=int(input("enter second number"))
+# print(a>=b)
+
+# print(10==10 and 10>5)
+
+
+# print(10==10 and 10>12)
+
+
+# print(10==1 and 10>5)
+
+
+# print(10==1 and 10>5)
+
+
+# print(10==10 or 10>5)
+
+# print(not(10==10 and 10<12))
+
+# a="welcome"
+# print('c' in a)
+
+# a="welcome"
+# print('b' in a)
+
+# a="welcome"
+# print('c' not in a)
+
+# a="welcome"
+# print('a'  not in a)
+
+# a=10
+# b=10
+# print(id(a))
+# print(id(b))
+# print(a is b)
+
+# a=10
+# b=12
+# print(id(a))
+# print(id(b))
+# print(a is b)
+
+# l=[1,2,3]
+# l1=[1,2,3]
+# print(id(l))
+# print(id(l1))
+# print(l is l1)
+
+# l=[1,2,3]
+# l1=[1,2,3]
+# print(id(l))
+# print(id(l1))
+# print(l is not l1)
+
+# a=10
+# b=10
+# print(id(a))
+# print(id(b))
+# print(a is not b)
+
+# a=int(input("enter first number"))
+# b=int(input("enter second number"))
+# if a>b:
+#     print("largest number is",a)
+# else:
+#     print("largest number is",b)    
+

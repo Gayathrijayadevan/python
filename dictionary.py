e0c2547aeb03973c1ef1619022ad7528c58b03ab
Gayathrijayadevan <gayathrjayadevan11@gmail.com>
2024-07-03 08:03:11 +0530

    july2

--- dictionary.py
+++ dictionary.py
@@ -7,7 +7,7 @@
 # for i in std:
     # print(i,std[i])
 
-std={'name':'akhil','age':21,'place':'ktm'}
+# std={'name':'akhil','age':21,'place':'ktm'}
 # print(std.get("name"))
 
 # print(std.items())
@@ -35,18 +35,36 @@ std={'name':'akhil','age':21,'place':'ktm'}
 # print(std.items())
 
 
-dtl=[]
-a=int(input("enter no. of students"))
-for i in a:
-    name=input("enter name:")
-    age=int(input("enter age:"))
-    course=input("enter course:")
-    place=input("enter course:")
-    dtl.append({'name':name,'age:'age,'course:'course,'place:'place})    
-print("{:<10}{:<6}".format("name","age","course","place"))
-print('_'*20)
-for i in dtl:
-    print(("{:<10}{:<6}").formate(i['name'],i['age'],i['course'],i['place']))
+# dtl=[]
+# a=int(input("enter no. of students"))
+# for i in a:
+    # name=input("enter name:")
+    # age=int(input("enter age:"))
+    # course=input("enter course:")
+    # place=input("enter course:")
+    # dtl.append({'name':name,'age:'age,'course:'course,'place:'place})    
+# print("{:<10}{:<6}".format("name","age","course","place"))
+# print('_'*20)
+# for i in dtl:
+    # print(("{:<10}{:<6}").formate(i['name'],i['age'],i['course'],i['place']))
+
+# 2/06/2024
+
+# number={0:'zero',1:'one',2:'two',3:'three',4:'four',5:'five',6:'six',7:'seven',8:'eight',9:'nine'}
+# a=int(input("enter a number:"))
+# result=""
+# while a>0:
+    # digit=a%10
+    # word=number[digit]
+    # result=word+''+result
+    # a=a//10
+    # print(result)
+    
+      
+
+
+
+
 
       
 
---
+++ function.py
@@ -0,0 +1,15 @@
+# def fun1():
+#     print('welcome')
+
+# fun1()    
+
+
+print('hai')
+def fun1():
+    print('welcome')
+
+
+print("all")
+fun1()    
+
+# def add():

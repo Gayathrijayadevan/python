a2a9c9e67eac67bbc6e7d3fd06f99067b7a99e23
Gayathrijayadevan <gayathrijayadevan11@gmail.com>
2024-07-09 11:29:14 +0530

    tuesday 9 july

---
+++ set.py
@@ -0,0 +1,44 @@
+# 9/07/2024
+
+# s={1,2,3,4,5,5}
+# print(s)
+
+# s=set()
+# print(s)
+
+# for i in s:
+    # print(i)
+
+# if 4 in s:
+    # print("yes")
+# else:
+    # print("no")
+
+# if 6 in s:
+    # print("yes")
+# else:
+    # print("no")
+# 
+
+# s.add(6)
+# print(s)
+
+# s.remove(6)
+# print(s)
+
+# s.discard(2)
+# print(s)
+
+# s.pop()
+# print(s)
+
+# s.update({'abc','def'})
+# print(s)
+
+# d=s.copy()
+# print(d)
+
+# s.clear()
+# print(s)
+
+

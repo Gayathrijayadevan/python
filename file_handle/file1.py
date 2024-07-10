ce9dd4266878dc89c6838d38b5e38e371df5553d
Gayathrijayadevan <gayathrijayadevan11@gmail.com>
2024-07-10 22:27:41 +0530

    file handiling

--- file_handle/file1.py
+++ file_handle/file1.py
@@ -7,10 +7,28 @@
 # f=open('python/file_handle/new4.txt','w')
 # f.write('hai')
 
-a=int(input("enter the number of students"))
-f=open('python/file_handle/new4.txt','w')
-for i in range(a):
-    n=input(("Enter name:"))
-    f.write(n)
+# a=int(input("enter the number of students"))
+# f=open('python/file_handle/new4.txt','w')
+# for i in range(a):
+#     n=input(("Enter name:"))
+#     f.write(n)
+
+# a=int(input("enter a number:"))
+# f=open('python/file_handle/new5.txt','w')
+# for i in range(1,11):
+#     n=i*a
+#     f.write(f"{i}*{a}={n}\n")
+
+# f = open("python/file_handle/new6.txt", "w")
+# a = int(input("Enter the number :"))
+# for i in range(1,a+1):
+#     for j in range(1, 11):
+#         b = str(j) + " * " + str(a) + " = " + str(j * a) + "\n"
+#         f.write(b)
+#     a-=1
+#     f.write('\n')
+
+
+
 
 

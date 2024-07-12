9ce77d06824ff9d47ebcdfe23a9de30dce3c3faa
Gayathrijayadevan <gayathrijayadevan11@gmail.com>
2024-07-12 11:30:26 +0530

    july12

---
+++ OOP/class&objects.py
@@ -0,0 +1,34 @@
+# class synnefo:
+#     def python():
+#         print("python")
+#     def mern():
+#         print("mern")
+# akhil=synnefo
+# akhil.python()        
+# akhil.mern()
+
+
+# class synnefo:
+#     def python(s):
+#         s.database='mysql'
+#         print("python")
+#     def mern(s):
+#         print("mern",s.database)
+# akhil=synnefo()
+# manu=synnefo()
+# akhil.python()        
+# manu.mern()
+
+class synnefo:
+    def __init__(s):
+        print("register")
+    def python(s):
+        s.database='mysql'
+        print("python")
+    def mern(s):
+        print("mern",s.database)
+akhil=synnefo()
+# manu=synnefo()
+# akhil.python()        
+# manu.mern()
+# 
---
+++ exception_handling/f1.py
@@ -0,0 +1,29 @@
+# while True:
+    # try:
+        # a= int (input("enter a number:"))
+        # print(a)
+        # 
+    # except:
+        # print("you should enter a number")    
+
+# l=[1,2,3,4.5,'abc',10]
+# n=0
+# for i in l:
+    # try:
+        # n+=i
+    # except:
+        # pass
+# print(n)        
+
+
+# try:
+    # print(10+'abc')
+# except NameError:
+    # print("nameerror")
+# except TypeError:
+    # print("type error")    
+# else:
+    # print('else')    
+# finally:
+    # print('finally')    
+
